10eec2d35bafeac6a5c7fad724bcf862d412f011
brandonekins <brandon.ekins@gmail.com>
2018-04-29 22:13:29 -0600

    finished the 7 lines

--- 358TheSevenSegments/TheSevenSegments.py
+++ 358TheSevenSegments/TheSevenSegments.py
@@ -1,8 +1,21 @@
-f = open('seven.txt','r')
-f = f.readlines()
-nums = []
-i = 0
-for idx, line in enumerate(f):
-    if idx % 3 == 2:
-        i += 1
-    
+#this function takes in the file name and then seperates it into a list of each number represented as a string of its parts
+def readNumbers(fileName):
+    f = open(fileName,'r')
+    f = f.readlines()
+    nums = ["" for i in range(9)] 
+    i = 0
+    for line in f:
+        for idx, c in enumerate(line[:-1]):
+            nums[i] +=c
+            if idx % 3 == 2:
+                i = i + 1
+        i = 0
+    return nums                  
+
+nums = readNumbers("seven.txt")
+#these are the strings in same format output by the readNumbers function in order by number
+numStrings = [' _ | ||_|','     |  |', ' _  _||_ ', ' _  _| _|', '   |_|  |', ' _ |_  _|', ' _ |_ |_|', ' _   |  |', ' _ |_||_|', ' _|_| _|']
+finalNum = ""
+for num in nums:
+   finalNum += str(numStrings.index(num)) 
+print(finalNum)
